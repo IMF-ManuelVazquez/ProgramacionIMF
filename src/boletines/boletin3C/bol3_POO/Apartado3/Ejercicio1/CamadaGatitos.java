09974985fc4a62debe73f1d2e80aabb6a9d877b9
Manuel <manuelve@gmail.com>
2020-04-17 23:02:17 +0200

    Update17_04_2020_3
    Cambio aplicado en la maquina Vritual

--- src/boletines/boletin3C/bol3_POO/Apartado3/Ejercicio1/CamadaGatitos.java
+++ src/boletines/boletin3C/bol3_POO/Apartado3/Ejercicio1/CamadaGatitos.java
@@ -2,17 +2,17 @@ package boletines.boletin3C.bol3_POO.Apartado3.Ejercicio1;
 
 public class CamadaGatitos {
 
-	Gato[] camada = new Gato[4];
+	Gato[] arrCamada = new Gato[4];
 	
 	public CamadaGatitos(Gato gato1, Gato gato2, Gato gato3, Gato gato4) {
-		camada[0]=gato1;
-		camada[1]=gato2;
-		camada[2]=gato2;
-		camada[3]=gato4;
+		arrCamada[0]=gato1;
+		arrCamada[1]=gato2;
+		arrCamada[2]=gato2;
+		arrCamada[3]=gato4;
 	}
 	
 	public void dameNombre(int pos) {
-		System.out.println(camada[pos].getNombreGato());
+		System.out.println(arrCamada[pos].getNombreGato());
 	}
 	
 }
