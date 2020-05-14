48853afceb4a59896f60a10b493c048605eaeeae
manuel <manuel>
2020-05-14 11:52:07 +0200

    Update14_05_2020

--- src/lecciones/T7_CONTROL_MANEJO_EXCEPCIONES/ExceptionRuntime.java
+++
@@ -1,9 +0,0 @@
-package lecciones.T7_CONTROL_MANEJO_EXCEPCIONES;
-
-public class ExceptionRuntime extends RuntimeException{
-	
-	protected ExceptionRuntime() {
-		// TODO Auto-generated constructor stub
-	}
-
-}
