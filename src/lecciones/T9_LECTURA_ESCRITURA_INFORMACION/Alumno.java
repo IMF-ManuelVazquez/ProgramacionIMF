bdd50c27a1c45a59c32db32a3b7f1995610e7253
manuel <manuel>
2020-05-22 18:06:16 +0200

    Update22_05_2020

---
+++ src/lecciones/T9_LECTURA_ESCRITURA_INFORMACION/Alumno.java
@@ -0,0 +1,39 @@
+package lecciones.T9_LECTURA_ESCRITURA_INFORMACION;
+
+import java.io.Serializable;
+
+public class Alumno implements Serializable{
+	
+	private String nombre;
+	private int edad;
+	private String dni;
+	
+	public Alumno(String nombre, int edad, String dni) {
+		super();
+		this.nombre = nombre;
+		this.edad = edad;
+		this.dni = dni;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public int getEdad() {
+		return edad;
+	}
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+	public String getDni() {
+		return dni;
+	}
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+	@Override
+	public String toString() {
+		return "Alumno [nombre=" + nombre + ", edad=" + edad + ", dni=" + dni + "]";
+	}
+}
---
+++ src/lecciones/T9_LECTURA_ESCRITURA_INFORMACION/UsandoSerializacion.java
@@ -0,0 +1,59 @@
+package lecciones.T9_LECTURA_ESCRITURA_INFORMACION;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+public class UsandoSerializacion {
+	
+	private final static String HOME = System.getProperty("user.home");
+	private static String pathname = HOME+"\\Desktop\\PROG_FICHEROS\\alumndfsdfo.dat";
+
+	public static void main(String[] args) {
+		
+		Alumno alumno1 = new Alumno("Manuel Vázquez", 22, "21312312z");
+		System.out.println(alumno1);
+		alumno1.setEdad(23);
+		System.out.println(alumno1);
+		
+		writeObject(alumno1);
+		
+		//Alumno alumno = readObject();
+		//System.out.println(alumno);
+		
+	}
+	
+	public static void writeObject(Alumno alumno) {
+		try {
+			ObjectOutputStream escribirObjetoAlumno = new ObjectOutputStream(new FileOutputStream(pathname));
+			escribirObjetoAlumno.writeObject(alumno);
+			escribirObjetoAlumno.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public static Alumno readObject() {
+		Alumno alumnoLeido = null;
+		try {
+			
+			ObjectInputStream lecturaObjetoAlumno = new ObjectInputStream(new FileInputStream(pathname));
+			alumnoLeido = (Alumno) lecturaObjetoAlumno.readObject();
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+		
+		return alumnoLeido;
+	
+	}
+
+}
